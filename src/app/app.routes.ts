3f2dde7e818211aedbdd8c22a044fac8d81e2b53
Jarod Cespedes <u202318588@upc.edu.pe>
2026-05-12 18:47:19 -0500

    feat(routes): add lazy loading for monitoring routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ const about = () => import('./shared/presentation/views/about/about').then((m) =
 const pageNotFound = () =>
   import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
 
+const monitoringRoutes = () => import('./monitoring/presentation/monitoring.routes').then((m) => m.monitoringRoutes);
+
 const baseTitle = 'BakeryManager';
 
 export const routes: Routes = [
   { path: 'home', component: Home, title: `${baseTitle} - Home` },
   { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
   // TODO: Add more paths here!
+  { path: 'monitoring', loadChildren: monitoringRoutes },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
 ];
