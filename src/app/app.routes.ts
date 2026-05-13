214af5cf7550f14f044a3d5ee6c37d011237855f
Jarod Cespedes <u202318588@upc.edu.pe>
2026-05-13 08:39:52 -0500

    chore: fix code syntax.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,6 @@ import { authenticationGuard } from './iam/infrastructure/guards/authentication.
 const about = () => import('./shared/presentation/views/about/about').then((m) => m.About);
 const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
 const inventoryManagement = () => import('./inventory/presentation/views/inventory-management/inventory-management').then((m) => m.InventoryManagement);
-
 const monitoringRoutes = () => import('./monitoring/presentation/monitoring.routes').then((m) => m.monitoringRoutes);
 
 const baseTitle = 'BakeryManager';
@@ -16,10 +15,10 @@ export const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: LoginPageComponent, title: `${baseTitle} - Sign In` },
   { path: 'sign-up', component: SignUpComponent, title: `${baseTitle} - Sign Up` },
+  { path: 'monitoring', loadChildren: monitoringRoutes },
   { path: 'inventory', loadComponent: inventoryManagement, canActivate: [authenticationGuard], title: `${baseTitle} - Inventory` },
   { path: 'home', component: Home, canActivate: [authenticationGuard], title: `${baseTitle} - Home` },
   { path: 'about', loadComponent: about, canActivate: [authenticationGuard], title: `${baseTitle} - About` },
-  { path: 'monitoring', loadChildren: monitoringRoutes },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
 ];
