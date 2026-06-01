77394b5eff8951510f715b66922aeea57b1642dd
AleDusty <u20221g231@upc.edu.pe>
2026-06-01 00:34:33 -0500

    refactor(environment): replace firebase config with inventory management api settings

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyB13aCI_-AWcwuSa9YPYoE6UQ_5CjOLiEc',
-    authDomain: 'bakerymanager.firebaseapp.com',
-    projectId: 'bakerymanager',
-    storageBucket: 'bakerymanager.firebasestorage.app',
-    messagingSenderId: '362412046305',
-    appId: '1:362412046305:web:6aadf14e30a688bf1d44a0',
-    measurementId: 'G-5LZXDVTP0E',
-  },
+
+  InventoryManagementApiBaseUrl: 'http://localhost:3000/api/v1',
+  InventoryManagementEndpointPath: '/Inventory',
 };
